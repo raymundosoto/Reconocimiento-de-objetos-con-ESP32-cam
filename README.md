b81f1dc891a1db5ae013b09a28b78f05edd13c05
M. En C. Raymundo Soto <raymundosotosoto@hotmail.com>
2022-12-29 14:34:22 -0600

    Archivos del proyecto
    Se suben todos los archivos del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Reconocimiento de objetos con ESP32 cam
- Este repositorio contiene la descripción del proyecto y archivos necesarios para reconocer objetos usando redes neuronales con el ESP32 CAM
+ Este repositorio contiene la descripción del proyecto y archivos necesarios para reconocer objetos usando redes neuronales con el ESP32 CAM.
+
+
+# Material necesario
+
+- Un microcontrolador ESP32CAM con cámara OV2640
+- Un cable USB a microUSB (se conectará el controlador a un puerto USB de la computadora)
+
+# Software necesario 
+
+Este proyecto se corrió en Linux en una máquina virtual con Ubuntu 22.04 pero se pueden seguir los mismos pasos en windows con las consideraciones necesarias para tener un ambiente de desarrollo adecuado.
+
+- IDE de Arduino 1.8.19 para programar el ESP32CAM
+- Anaconda para linux 
+- Editor de código para Python de su preferencia (Visual estudio code, Sublime text, etc.)
+- Todos los programas del repositorio 
+
+# Instalación del proyecto
+
+- Instalar IDE de Arduino [Guía de instalación](https://ubunlog.com/arduino-ide-entorno-desarrollo-para-trabajar-con-arduino/?utm_source=dlvr.it&utm_medium=twitter)
+
+- Instalar Anaconda [Guía de instalación de Anaconda](https://noviello.it/es/como-instalar-anaconda-en-ubuntu-22-04/)
+
+En todas las instrucciones a continuación se considera que se conoce el manejo de la terminal de Linux por lo que las instrucciones se escribe directamente en la terminal. 
+
+- Crear un carpeta con el nombre que se quiera para el proyecto
+- Abrir una terminal en la carpeta y clonar el repositorio con todos los archivos
+
+´git clone https://github.com/raymundosoto/Reconocimiento-de-objetos-con-ESP32-cam´
+
+# Instrucciones de programación
+
+# Instrucciones de uso
+
+# Resultados
+ 
---
+++ reconocimientoESP32.py
@@ -0,0 +1,52 @@
+import cv2 #Importar OpenCv
+import urllib.request #Librería para para abrir y leer la URL del servidor web del ESP32 CAM
+import numpy as np
+
+#PROGRAMA DE CLASIFICACION DE OBJETOS PARA VIDEO EN DIRECCION IP 
+
+url = 'http://192.168.100.142/capture'    #Aquí se debe insertar la IP que arroja el monitor serial del IDE de Arduino
+#url = 'http://192.168.1.6/'
+winName = 'Cámara ESP32'
+cv2.namedWindow(winName,cv2.WINDOW_AUTOSIZE)
+
+
+classNames = []
+classFile = 'coco.names'  #Archivo con los nombres de los objetos a reconocer
+with open(classFile,'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'  #Archivos con los pesos de la red neuronal
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while(1):
+    imgResponse = urllib.request.urlopen (url) #abrimos el URL con las imágenes tomadas del ESP32
+    imgNp = np.array(bytearray(imgResponse.read()),dtype=np.uint8)
+    img = cv2.imdecode (imgNp,-1) #decodificción de la imágenes
+
+    #img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE) # Descomentar esta línea si se quiere la imagen vertical
+    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Descomentar si se quieren lasm imáganes en escala de grises
+
+    
+
+    classIds, confs, bbox = net.detect(img,confThreshold=0.5)   #Clasificación de los objetos
+    print(classIds,bbox)
+
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness = 3) #mostramos en rectangulo lo que se encuentra
+            cv2.putText(img, classNames[classId-1], (box[0]+10,box[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0,255,0),2)
+
+
+    cv2.imshow(winName,img) # mostramos la imagen
+
+    #esperamos a que se presione ESC para terminar el programa
+    tecla = cv2.waitKey(5) & 0xFF
+    if tecla == 27:
+        break
+cv2.destroyAllWindows()
